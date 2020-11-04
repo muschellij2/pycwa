ca855bf5019bf7ed619bf6b25ae326a042ebd006
muschellij2 <muschellij2@gmail.com>
2020-11-04 18:29:24 -0500

    trying to figure out java

--- tests/testthat/test-aaa_python.R
+++ tests/testthat/test-aaa_python.R
@@ -1,5 +1,9 @@
 testthat::test_that("install requirements", {
   testthat::skip_on_cran()
+  pycwa:::cp_java()
+  acc_file = system.file("acc", "java", "AccelerometerParser.class",
+                         package = "pycwa")
+  testthat::expect_true(file.exists(acc_file))
   if (!pycwa:::have_python_requirements() &
       reticulate::py_available(initialize = FALSE)) {
     pycwa:::install_python_requirements()
